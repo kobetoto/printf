2a41d316f1cafd8dcf2ca9a3bcc19cf7d7d2a1c4
Thomas <thodavid@student.42.fr>
2025-01-08 15:10:06 +0100

    test implementation + buggy behavior ft_printf.c (output > stdout)

--- ft_printf.c
+++ ft_printf.c
@@ -1,35 +1,99 @@
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 10:32:46 by thodavid          #+#    #+#             */
+/*   Updated: 2025/01/08 15:03:02 by thodavid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int print(char *s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+	{
+		write(1,&s[i],1);
+		i++;
+	}
+	return (i);
+}
 
 int ft_printf(const char *fmt, ...)
 {
-        va_list ap;
-        int     nbr_arg = 0;
-        int     i = 0;
-        char    *s = (char *)fmt;
+	va_list	ap;
+	int	char_print = 0;
+	char *s = (char *)fmt;
 
-        va_start(ap, fmt);
+	print(s);
+	
+	//void va_start(va_list ap, last);
+	va_start(ap, fmt);
+	
+	//type va_arg(va_list ap, type);
+	//printf("\n\nF s1 is ==%s==\n\n", va_arg(ap, char *));
+  	//printf("F s2 is ==%s==\n\n", va_arg(ap, char *));
+    	//printf("F s3 is ==%s==\n\n", va_arg(ap, char *));
 
-        //Each call to va_arg() modifies ap so that the next call returns the next argument.
-        while(i < 3)
-        { 
-        s = va_arg(ap, char *);
-        printf("s is ==%s==\n\n", s);
-        nbr_arg--;
-        i++;
-        }
-        va_end(ap);     
-        return nbr_arg;
+        va_end(ap);	
+	return char_print;
 }
 
 int main(int argc, char **argv)
 {
-        (void) argc;
+	(void) argc;
+	(void) argv;
 
-        //char *s1 = "test";
-        //char *s2 = "testtttt";
+	//TEST1
+ printf("\n\n===\nTEST1\n===\n\n");
+ printf("printf is ---->       ");
+ printf("Hello Worldd\n");
 
-        //printf("printf is ==%s==\n", argv[1]);
-        ft_printf(argv[1], argv[2], argv[3]);
-        return 0;
+ printf("ft_printf is ---->       ");
+ ft_printf("Hello worl\n");
+
+
+        // //TEST2
+        // printf("\n\n===\nTEST2\n===\n\n");
+        // printf("printf is ---->       ");
+        // printf(" ");
+
+        // printf("ft_printf is ---->       ");
+        // ft_printf(" ");
+
+
+        // //TEST3
+        // printf("\n\n===\nTEST3\n===\n\n");
+        // printf("printf is ---->       ");
+        // printf("%");
+
+        // printf("ft_printf is ---->       ");
+        // ft_printf("%");
+
+
+	// //TEST4
+        // printf("\n\n===\nTEST4\n===\n\n");
+        // printf("printf is ---->       ");
+        // printf("");
+
+        // printf("ft_printf is ---->       ");
+        // ft_printf("");
+
+
+	// //TEST5
+        // printf("\n\n===\nTEST5\n===\n\n");
+        // printf("printf is ---->       ");
+	// printf(argv[1], argv[2], argv[3]);
+
+        // printf("ft_printf is ---->       ");
+	//ft_printf(argv[1], argv[2], argv[3], argv[4]);
+	
+	return 0;
 }
