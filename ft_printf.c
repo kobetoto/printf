3e50d5b95a0068feddadcde5086218f26e5d90ad
Thomas <thodavid@student.42.fr>
2025-01-10 14:47:09 +0100

    arg: OK print: OK
    ./a.out arg[1] arg[2] arg[3]
    >printf in second
    >stdout of write(1,,1) first

--- ft_printf.c
+++ ft_printf.c
@@ -6,31 +6,18 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 10:32:46 by thodavid          #+#    #+#             */
-/*   Updated: 2025/01/08 15:03:02 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/01/10 14:36:48 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int print(char *s)
-{
-	int	i;
-
-	i = 0;
-	while(s[i])
-	{
-		write(1,&s[i],1);
-		i++;
-	}
-	return (i);
-}
+#include "ft_printf.h"
 
 int ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
+	char	*s;
 	int	char_print = 0;
-	char *s = (char *)fmt;
+
+	s = (char *)fmt;
 
 	print(s);
 	
@@ -38,9 +25,9 @@ int ft_printf(const char *fmt, ...)
 	va_start(ap, fmt);
 	
 	//type va_arg(va_list ap, type);
-	//printf("\n\nF s1 is ==%s==\n\n", va_arg(ap, char *));
-  	//printf("F s2 is ==%s==\n\n", va_arg(ap, char *));
-    	//printf("F s3 is ==%s==\n\n", va_arg(ap, char *));
+	printf("\n\nF s1 is ==%s==\n\n", va_arg(ap, char *));
+  	printf("F s2 is ==%s==\n\n", va_arg(ap, char *));
+    	printf("F s3 is ==%s==\n\n", va_arg(ap, char *));
 
         va_end(ap);	
 	return char_print;
@@ -49,15 +36,14 @@ int ft_printf(const char *fmt, ...)
 int main(int argc, char **argv)
 {
 	(void) argc;
-	(void) argv;
+	//(void) argv;
 
 	//TEST1
  printf("\n\n===\nTEST1\n===\n\n");
- printf("printf is ---->       ");
+ printf("printf    ---->       ");
  printf("Hello Worldd\n");
-
- printf("ft_printf is ---->       ");
- ft_printf("Hello worl\n");
+ printf("ft_printf ---->       ");
+ ft_printf("Hello world\n", argv[1], argv[2], argv[3]);
 
 
         // //TEST2
