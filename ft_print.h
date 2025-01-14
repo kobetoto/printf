df326f24d19a807a826aad5c60fbc74a5190ebe9
Thomas <thodavid@student.42.fr>
2025-01-14 14:36:00 +0100

    Update ft_print.h > new file

--- ft_print.h
+++ ft_print.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print.c                                            :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thodavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/10 11:09:34 by thodavid          #+#    #+#             */
-/*   Updated: 2025/01/12 14:28:59 by thodavid         ###   ########.fr       */
+/*   Created: 2025/01/10 14:14:19 by thodavid          #+#    #+#             */
+/*   Updated: 2025/01/14 14:26:35 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,6 +14,9 @@
 #include <stdarg.h>
 
 int ft_printf(const char *fmt, ...);
-size_t ft_putchar(char c);
+
+size_t ft_putchar(const char c);
 size_t ft_putstr(const char *s);
-size_t ft_putnbr(int n);
+size_t ft_putnbr(const int nb);
+size_t ft_unsputnbr(const unsigned int nb);
+size_t ft_puthexa(const unsigned int nb);
