ce0e3dfc2eb9153ea80e1b5841c2960cf317fe0e
Thomas <thodavid@student.42.fr>
2025-01-15 14:03:37 +0100

    version f. > norme OK

--- ft_print.h
+++ ft_print.h
@@ -6,17 +6,25 @@
 /*   By: thodavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 14:14:19 by thodavid          #+#    #+#             */
-/*   Updated: 2025/01/14 14:26:35 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:36:38 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdarg.h>
+#	ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-int ft_printf(const char *fmt, ...);
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-size_t ft_putchar(const char c);
-size_t ft_putstr(const char *s);
-size_t ft_putnbr(const int nb);
-size_t ft_unsputnbr(const unsigned int nb);
-size_t ft_puthexa(const unsigned int nb);
+int		ft_printf(const char *fmt, ...);
+
+size_t	ft_putchar(const char c);
+size_t	ft_putstr(const char *s);
+size_t	ft_putnbr(const int nb);
+size_t	ft_unsputnbr(const unsigned int nb);
+size_t	ft_puthexa(const unsigned int nb);
+size_t	ft_puthexaupp(const unsigned int nb);
+size_t	ft_printaddr(const unsigned int nb);
+size_t	ft_putadress(void *adress);
+
+#endif
